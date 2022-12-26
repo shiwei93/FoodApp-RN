e7845f563cad68619bb2ef4bc572f3567c25d6c6
shiwei93 <stayfocusjs@gmail.com>
2022-12-26 19:54:05 +0800

    添加应用内图片

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Image } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 import Home from '@/pages/home';
+import Colors from '../assets/colors';
+
+Feather.loadFont();
 
 const Stack = createNativeStackNavigator();
 
@@ -12,12 +17,29 @@ const App = () => {
                     headerShadowVisible: false,
                 }}
             >
-                <Stack.Group screenOptions={{ headerStyle: { backgroundColor: '#F9F9FB' } }}>
+                <Stack.Group
+                    screenOptions={{ headerStyle: { backgroundColor: Colors.background } }}
+                >
                     <Stack.Screen
                         name="Home"
                         component={Home}
                         options={{
                             title: '',
+                            headerLeft: () => {
+                                return (
+                                    <Image
+                                        style={{
+                                            width: 40,
+                                            height: 40,
+                                            borderRadius: 20,
+                                        }}
+                                        source={require('assets/images/profile.png')}
+                                    />
+                                );
+                            },
+                            headerRight: () => {
+                                return <Feather name="menu" size={24} color={Colors.textDark} />;
+                            },
                         }}
                     />
                 </Stack.Group>
